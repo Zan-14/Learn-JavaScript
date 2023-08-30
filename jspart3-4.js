f64e311ca726b9497c794690b6f43cd11a0e88d4
Zan-14 <fauzanjumdinharm@gmail.com>
2023-08-30 18:05:10 +0700

    progress minitugas, Quiz not yet

--- jspart3-4.js
+++ jspart3-4.js
@@ -88,12 +88,17 @@ console.log(cityBig, citySmall);
 
 
 // trim (to erase useless space)
-
+const noTrimCity = "   Blora Indah Indonesia          "
+console.log(noTrimCity, 'before trim');
+console.log(noTrimCity.length, 'length before trim'); // before trim
+const trimCity = noTrimCity.trim();
+console.log(trimCity); // after trim
+console.log(trimCity.length, 'length after trim'); // after trim
 
 
 //split = to make array of strings
-const fruit = "banana,apple,orange,melon";
-const arrayFruit = fruit.split(',');
+const fruit = "banana,apple,orange,melon"; //this is a string
+const arrayFruit = fruit.split(','); // this will create a new array with ',' as separator
 console.log(arrayFruit);
 
 // Object >> {}
@@ -112,19 +117,25 @@ const userIdentity2 = {
     usia: 25,
     asal: "Semarang"
 }
+// If we have a lot of users, we don't type all of them. We use OOP instead. Check the OOP meaning in the next session
+
+// JSON file is also in the form of object data
+// JSON (JavaScript Object Notation) 
+
 console.log(userIdentity1);
 console.log(userIdentity2);
-console.log(userIdentity1.usia);
+console.log(userIdentity1.usia); //first method to call a data from object
 console.log(userIdentity2.username);
+console.log(userIdentity1["motto hidup"]); //second method to call a data from object (use this if the key name is more than 1 word)
 console.log(userIdentity2["username"]);
-console.log(userIdentity1["motto hidup"]);
 
-console.log(userIdentity2["nama lengkap"].firstName);
+console.log(userIdentity2["nama lengkap"].firstName); // calling a data inside an object, inside an object
 
 //Array 
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 console.log(numbers);
 
+//Object inside array
 const userList = [
     {
         username: "Adi",
@@ -143,39 +154,212 @@ const userList = [
     },
 ];
 console.log(userList);
-console.log(userList[1].username);
+console.log(userList[1].username); //return = Hikam
 
+
+// map() = to call certain data and create a new data
 const changeUsia = userList.map((item) => {
     return {
         age: item.usia
     }
 })
-console.log(changeUsia);
+console.log(changeUsia, 'only Usia will appear and create a new array');
 
-// https://github.com/prasetya12/batch_8
+const showAsal = userList.map((free) => {
+    return {
+        kota: free.asal,
+    }
+});
+console.log(showAsal, 'only show asal data with a change on the word asal to kota');
 
 
-//JS Part 4
+// You have completed the JavaScript part 3 (session 16)
+
+
+/*
+PART 4 
+*/
 
 
 // Date and Time
-// Januari = 0 not 1
-// Januari = 0 not 1
+
 
 
 const bulan = ["Januari", "Februari", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-const now = new Date();
-console.log(now);
-console.log(now.getMonth() + 1);
-console.log(now.getDay());
+const now = new Date(); // how to declare the date
 
-// const formatIndo = now.toLocaleDateString("id-ID", options);
-// console.log(formatIndo); //kenapa error???
+/* Index of month and day
+Januari = 0 
+Sunday = 0 (the day start from Sunday)
+Midnight = 0, so range of hour is 0-23
+*/
+console.log(now); // how to show current date inside console
+console.log(now.getSeconds(), 'current seconds');
+console.log(now.getMinutes(), 'current minutes');
+console.log(now.getHours(), 'current hour');
+console.log(now.getMonth() + 1, 'current month'); // how to show current month (in number)
+console.log(now.getDay() + 1, 'current day');
+const nowYearOnly = new Date();
+console.log(nowYearOnly.getFullYear(), 'current year');
 
-// const formatTimeIndo = now.toLocaleString('id-ID', options);
-// console.log(formatTimeIndo);
+//Change the date format
+// 1. Set to local format
+let formatIndo = now.toLocaleDateString('id-ID');
+console.log(formatIndo, '"format local (Indonesia)"'); // show local time
+
+// 2. change month format to letter
+let options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+}
+let formatIndoLong = now.toLocaleDateString('id-ID', options);
+console.log(formatIndoLong, '"Month to letter format"');
+
+// 3. add hour.minutes.seconds
+let formatIndoFull = now.toLocaleTimeString('id-ID', options);
+console.log(formatIndoFull, '"full Indo format"');
+
+/* How to show current time in HTML real time */
+let realTime = document.getElementById('current-time');
+// declare and get the HTML element's id
+setInterval(() => {
+    let showTime = new Date();
+    realTime.innerHTML = showTime.toLocaleTimeString('id-ID', options);
+}, 1000);
 
 
-const baru = new Date (1999, 2, 1)
-console.log(baru);
+// ASSIGNMENT (minitugas.js)
+// First Data
+const dataMahasiswa = [
+    {
+        id: 1,
+        nama: "Alice",
+        usia: 20,
+        jurusan: "Informatika"
+    },
+    {
+        id: 2,
+        nama: "Bob",
+        usia: 22,
+        jurusan: "Teknik Elektro"
+    },
+    {
+        id: 3,
+        nama: "Carol",
+        usia: 21,
+        jurusan: "Desain Grafis"
+    },
+    {
+        id: 4,
+        nama: "Balmon",
+        usia: 21,
+        jurusan: "Informatika"
+    }
+];
+console.log(dataMahasiswa);
+
+//1. Buatkan data diatas menjadi seperti ini ["Alice","Bob","Carol", "Balmon"]
+
+// first method
+const nameOnly = dataMahasiswa.map((user) => user.nama);
+console.log(nameOnly, 'first method');
+
+// second method
+const nameOnly2 = dataMahasiswa.map((user) => {
+    return user.nama
+});
+console.log(nameOnly2, 'second method');
+
+
+//2. Buatkan array object yang hanya menampilkan nama dan usia saja [{nama:,usia},{....}]
+const ArrNameAge = dataMahasiswa.map((nameAge) => {
+    return {
+        nama: nameAge.nama,
+        usia: nameAge.usia,
+    }
+})
+console.log(ArrNameAge);
+
+
+/* 3. Tampilkan pada html dengan tampilan seperti ini
+1. nama: Alice - 20
+2. nama: Alice - 20
+*/
+const showDataNameAge = document.getElementById('show-object-data');
+dataMahasiswa.map((data) => {
+    showDataNameAge.innerHTML += `<li>${data.id}. nama:${data.nama} - ${data.usia}</li>`;
+});
+
+
+// Second Data
+const dataMahasiswaLengkap = [
+    {
+        id: 1,
+        nama: "Alice",
+        usia: 20,
+        jurusan: "Informatika",
+        alamat: {
+            kota: "Jakarta",
+            provinsi: "DKI Jakrta"
+        },
+        nilaiAkhir: 85,
+    },
+    {
+        id: 2,
+        nama: "Bob",
+        usia: 22,
+        jurusan: "Teknik Elektro",
+        alamat: {
+            kota: "Jogja",
+            provinsi: "DI Yogyakarta"
+        },
+        nilaiAkhir: 90,
+
+    },
+    {
+        id: 3,
+        nama: "Carol",
+        usia: 21,
+        jurusan: "Desain Grafis",
+        alamat: {
+            kota: "Semarang",
+            provinsi: "Jawa Tengah"
+        },
+        nilaiAkhir: 70,
+
+    },
+    {
+        id: 4,
+        nama: "Balmon",
+        usia: 21,
+        jurusan: "Informatika",
+        alamat: {
+            kota: "Malang",
+            provinsi: "Jawa Timur"
+        },
+        nilaiAkhir: 60,
+
+    }
+]
+console.log(dataMahasiswaLengkap);
+
+//1. Buatkan data datas menjadi seperti ini [{nama:"Bob",kotaAsal:"Jogja","Kalimat":"Saya berasal dari malang"}]
+const mhsNamaKotaSapaan = dataMahasiswaLengkap.map((data) => {
+    return {
+        nama: data.nama,
+        'kota asal': data.alamat.kota,
+        'sapaan': `Saya berasal dari ${data.alamat.kota}`,
+    }
+});
+console.log(mhsNamaKotaSapaan);
+
+/*2. Tampilkan pada html mahasiswa yang jurusan informatika, dan munculkan nama dan provinsi.
+Example:
+1. Balmon - Jawa Timur
+*/
+
+
+/* 3. Munculkan pada alert berapa rata rata nilaiAkhir dari data mahasiswa diatas
+*/
